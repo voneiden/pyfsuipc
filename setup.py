2604fd4a7e9e7e775da417bf2585c42e6f4c0309
brightening-eyes <amir.ramezani1370@gmail.com>
2019-06-04 14:06:45 +0430

    tried to make setup.py compatible with pep-8

--- setup.py
+++ setup.py
@@ -4,6 +4,8 @@ from Cython.Build import cythonize
 
 setup(
     ext_modules = cythonize([
-    Extension("pyfsuipc", sources=["pyfsuipc.pyx", "IPCuser.c"], libraries=["advapi32", "user32", "kernel32", "ole32", "oleaut32", "gdi32", "gdiplus", "imm32"])
+    Extension("pyfsuipc", sources=["pyfsuipc.pyx", "IPCuser.c"],
+libraries=["advapi32", "user32", "kernel32", "ole32", "oleaut32", "gdi32",
+"gdiplus", "imm32"])
     ])
 )
